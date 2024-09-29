d3b12ed5a21835999ef76d1083b1d862d00b87f5
Robert Khuc <khucthienphuc@gmail.com>
2024-09-29 12:18:18 +0700

    Update service structure: move services into 'services' folder (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { MenuModule } from './menu/menu.module';
-import { BannerModule } from './banner/banner.module';
-import { FoodModule } from './food/food.module';
-import { StoreModule } from './store/store.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { CartModule } from './cart/cart.module';
+import { MenuModule } from './services/menu/menu.module';
+import { BannerModule } from './services/banner/banner.module';
+import { FoodModule } from './services/food/food.module';
+import { StoreModule } from './services/store/store.module';
+import { UserModule } from './services/user/user.module';
+import { CartModule } from './services/cart/cart.module';
+import { AuthModule } from './services/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, MenuModule, BannerModule, FoodModule, StoreModule, AuthModule, UserModule, CartModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    MenuModule,
+    BannerModule,
+    FoodModule,
+    StoreModule,
+    AuthModule,
+    UserModule,
+    CartModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
